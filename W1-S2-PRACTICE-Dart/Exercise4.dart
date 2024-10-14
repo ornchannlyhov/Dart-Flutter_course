ee13928f8fc327171fbc646d4bb1acec859bce88
lyhov <ornchannlyhov@gmail.com>
2024-10-14 10:50:02 +0700

    change Exercise4 to function based

--- W1-S2-PRACTICE-Dart/Exercise4.dart
+++ W1-S2-PRACTICE-Dart/Exercise4.dart
@@ -1,18 +1,24 @@
-void main() {
+String order(List<String> pizza_list) {
   const pizzaPrices = {
     'margherita': 5.5,
     'pepperoni': 7.5,
     'vegetarian': 6.5,
   };
-  const order = ['margherita', 'pepperoni'];
 
   double total = 0;
-  for (var pizza in order) {
+  for (var pizza in pizza_list) {
     if (pizzaPrices.containsKey(pizza)) {
       total += pizzaPrices[pizza]!;
     } else {
-      print('$pizza is not on the menu');
+      return ('$pizza is not on the menu');
     }
   }
-  print('Total: \$${total}');
+
+  return 'Total: \$${total.toStringAsFixed(2)}';
+}
+
+void main() {
+  const orderList = ['vegetarian', 'pepperoni'];
+
+  print(order(orderList));
 }
